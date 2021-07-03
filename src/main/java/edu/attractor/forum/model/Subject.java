51e071acc81bf5b6968aa0ec313c438de0f894f7
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-07-03 11:33:38 +0600

    Filled in models

--- src/main/java/edu/attractor/forum/model/Subject.java
+++ src/main/java/edu/attractor/forum/model/Subject.java
@@ -1,4 +1,36 @@
 package edu.attractor.forum.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Table(name="subjects")
 public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    private String events;
+
+    private LocalDate localDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public Subject(@NotBlank @Size(min = 1, max = 128) String events, LocalDate localDate, User user) {
+        this.events = events;
+        this.localDate = localDate;
+        this.user = user;
+    }
 }
--- src/main/java/edu/attractor/forum/model/User.java
+++ src/main/java/edu/attractor/forum/model/User.java
@@ -1,4 +1,47 @@
 package edu.attractor.forum.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Email
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 128)
+    @Column(length = 128)
+    private String password;
+
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    private String username;
+
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    @Builder.Default
+    private String role = "USER";
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Subject> subject;
 }
